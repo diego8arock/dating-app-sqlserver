fbee8899eb5e4e6f9324e15dff7e9a688b6eef15
Diego Ochoa <diego-ochoa@pop-os.localdomain>
2020-04-16 11:06:54 -0500

    modify UI theme

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -8,10 +8,16 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent],
 })
