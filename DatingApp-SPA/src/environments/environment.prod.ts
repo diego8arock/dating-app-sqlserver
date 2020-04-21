8d12d26fd467a8b9e1ab5a519043ee45267447c7
Diego Ochoa <diego-ochoa@pop-os.localdomain>
2020-04-20 19:39:18 -0500

    adjust api url name variable

--- DatingApp-SPA/src/environments/environment.prod.ts
+++ DatingApp-SPA/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-udemy-training.azurewebsites.net/api/',
+  apiUrl: 'https://api-udemy-training.azurewebsites.net/api/',
 };
